cb47882dfcdfe8cced481f96b5a94abc2cf0b0e4
RodrigoXrdm <rodrigooa17@gmail.com>
2017-05-18 20:41:14 -0300

    Update state.h

--- Testes/state.h
+++ Testes/state.h
@@ -7,11 +7,25 @@ using namespace std;
 
 struct state{
     String nome;
-    list<state> preposicao;
+    list<char> proposition;
 
     estado(string nome){
         this->nome = nome;
     }
+
+    list<char> getStateProposition(state s){
+          return s.proposition;
+    }
+
+    bool existProposition(char p){
+        foreach(char aux, this->proposition) {
+            if(aux == p)
+                return true;
+        }
+
+        return false;
+    }
+
 };
 
 #endif // ESTADO_H
